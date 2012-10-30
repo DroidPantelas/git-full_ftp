d5055a27407b8c387831da6e6fd540cf37100f27
Amol Mandhane <manamol296@gmail.com>
2012-10-31 04:06:53 +0530

    Start page created

---
+++ toolbox.py
@@ -0,0 +1,22 @@
+import web
+
+BASE_URL = "http://localhost:8080/"
+STATIC_URL = "http://localhost:8080/static/"
+BASE_TITLE = "Git-full FTP"
+VIEWS_DIR = 'templates/'
+LAYOUT_NAME = 'layout'
+render_engine = web.template.render(VIEWS_DIR, base=LAYOUT_NAME)
+
+
+def render(view, data=None, title=''):
+	if data is None:
+		data = {}
+	if title is '':
+		title = BASE_TITLE
+	else:
+		title = title + " - " + BASE_TITLE
+
+	data['title'] = title
+	data['base_url'] = BASE_URL
+	data['static_url'] = STATIC_URL
+	return eval("render_engine." + str(view) + "(data)")
