0cec5e963f0700421cd56a00567a1f2f64498331
Amol Mandhane <manamol296@gmail.com>
2012-10-31 03:17:59 +0530

    Templating

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 import web
+
+urls = (
+	"/", "index"
+)
+
+render = web.template.render('templates', base="layout")
+
+
+class index:
+	def GET(self):
+		return render.index()
+
+if __name__ == "__main__": 
+	app = web.application(urls, globals())
+	app.run()
