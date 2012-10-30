3285d0257e469b4a1d5688d0ddf30fb8f87fca3c
Amol Mandhane <manamol296@gmail.com>
2012-10-31 04:05:26 +0530

    Start page created

--- app.py
+++ app.py
@@ -4,12 +4,31 @@ urls = (
 	"/", "index"
 )
 
-render = web.template.render('templates', base="layout")
+BASE_URL = "http://localhost:8080/"
+STATIC_URL = "http://localhost:8080/static/"
+BASE_TITLE = "Git-full FTP"
+VIEWS_DIR = 'templates/'
+LAYOUT_NAME = 'layout'
+render_engine = web.template.render(VIEWS_DIR, base=LAYOUT_NAME)
+
+
+def render(view, data=None, title=''):
+	if data is None:
+		data = {}
+	if title is '':
+		title = BASE_TITLE
+	else:
+		title = title + " | " + BASE_TITLE
+
+	data['title'] = title
+	data['base_url'] = BASE_URL
+	data['static_url'] = STATIC_URL
+	return eval("render_engine." + str(view) + "(data)")
 
 
 class index:
 	def GET(self):
-		return render.index()
+		return render("index")
 
 if __name__ == "__main__": 
 	app = web.application(urls, globals())
