85e977b9f01a119dd023a8b917d8e62e7a8b57a7
Amol Mandhane <manamol296@gmail.com>
2012-11-27 14:54:03 +0530

    Webhook support

--- app.py
+++ app.py
@@ -116,6 +116,73 @@ class sync:
         return data['uid']
 
 
+        def POST(self):
+            data = web.input()  # GET data
+            config = get_data(data) # Get the decrypter server and git config from encrypter.py
+
+            ############
+            # Downloading the zip file of the repo
+            ##############
+
+            buff = urllib2.urlopen(config['zip'])
+            f = open(absolute_path("tmp/" + (config['zip'].split("/")).pop()), "wb") #### Writing it to tmp/master.zip
+            f.write(buff.read())
+            f.close()
+
+            ###########
+            # Download finished
+            ###########
+
+
+            ###########
+            # Extract the zipfile to same folder
+            ###########
+
+            repo = zipfile.ZipFile(absolute_path("tmp/" + (config['zip'].split("/")).pop()))
+            repo.extractall(absolute_path("tmp/"))
+            local_dirname = repo.filelist[0].filename.replace("/","")
+            repo.close()
+            os.remove(absolute_path("tmp/" + (config['zip'].split("/")).pop()))
+            
+            ###########
+            # Extraction finished. Zip file deleted.
+            ############
+
+            ##################
+            # Getting username, password, host, port from FTP_URL
+            ##################
+
+            ftp_url = config['ftp']
+            url = ftp_url.replace("ftp://","")
+            if url[-1] == "/":
+                url = url[:-1]
+
+            tmp1 = url.split(":",1)
+            username = tmp1[0]
+            tmp2 = reverse(tmp1[1]).split("@",1)
+            password = reverse(tmp2.pop())
+            host_port = reverse(tmp2[0])
+            host, port = host_port.split(":")
+
+            ##### End
+
+
+
+            ##############
+            # Synchronize
+            ##############
+
+            s = synchronizer(host, port, username, password, absolute_path("tmp/" + local_dirname), config['dir'])
+            s.sync()
+
+            #### Done
+
+
+            #### Delete the extracted directory
+            delete_dir(absolute_path("tmp/" + local_dirname))
+
+            return data['uid']
+
 if __name__ == "__main__": 
     app = web.application(urls, globals())
     app.run()
